e3b867c984d70925bd559bc322472abd6da2ea3c
ManuelCalleros_ <33211111+jesus2332@users.noreply.github.com>
2023-04-04 12:41:07 -0600

    Ejercicio 32
    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
    debiendo ir cada uno en una fila distinta.

---
+++ ej32.js
@@ -0,0 +1,13 @@
+
+//Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
+//debiendo ir cada uno en una fila distinta. 
+
+let names = ["Florinda", "Fernando", "Fernanda", "federico", "Jose", 
+"Jesus", "Javier", "Juan", "Jorge", "Joaquin",
+ "Julieta", "Irving", "Jessica", "Isidro", "Ignacio",
+"Sasuke", "Gilberto", "Gustavo", "Gabriel", "Anahi"]
+
+for (let index = 0; index <names.length; index++) {
+    console.log(names[index])
+    
+}
